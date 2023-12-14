1e8b5b6408192794dccd842987f61b0752e7045f
Elizabeth Mann <elma8936@colorado.edu>
2023-12-14 13:22:15 -0700

    new memory game

--- jsProject/jsProject.js
+++ jsProject/jsProject.js
@@ -1,25 +1,25 @@
-//diplay vs visibil
-
-//cardOne.style.display  = none;
-//cardOne.style.visbility = "hidden";
-
-//query Slectors to make buttons 
 const card = document.querySelectorAll(".back");
 const dontTouch = document.querySelector(".dontTouch");
 
 const btn = document.querySelector(".button");
 let phoneNumerTxt = document.querySelector('#phoneNumberTxt'); 
-let submitBtn = document.querySelector(".submit")
+let submitBtn = document.querySelector(".submit");
 
-//initalize values
+// const cardFlip = document.getElementsByClassName("card");
+
+// cardFlip.addEventListener("click", test);
+
+// function test(){
+//   cardFlip.classList.toggle("flipCard");
+//}
+
 let flippedCards = 0;
 let firstCard;
 let secondCard;
 
-//event listeners
 card.forEach(card => {
     card.addEventListener("click", flipCard);
-  });
+});
 
 btn.addEventListener("click", shuffleCards);
 
@@ -27,8 +27,8 @@ submitBtn.addEventListener("click", submit)
 
 //functions 
 function shuffleCards(){
-    console.log("reset");
-    phoneNumerTxt.textContent = "";
+  console.log("reset");
+  phoneNumerTxt.textContent = "";
 }
 
 function flipCard(e){
@@ -36,7 +36,6 @@ function flipCard(e){
     submit();
     return;
   }
-
   if(flippedCards === 0){
     firstCard = e.target;
     flippedCards += 1;
@@ -45,7 +44,8 @@ function flipCard(e){
   else{
     secondCard = e.target;
     secondCard.style.zIndex = "-9000";
-    if((firstCard.getAttribute("cardNumber") === (secondCard.getAttribute("cardNumber")))){
+
+    if(firstCard.getAttribute("cardNumber") === (secondCard.getAttribute("cardNumber"))){
       if(phoneNumberTxt.textContent.length === 3 ||  phoneNumberTxt.textContent.length === 9){
         phoneNumberTxt.textContent += " - ";
       }
@@ -57,9 +57,10 @@ function flipCard(e){
     }
     //cant click
     //dontTouch.style.display = "none";
-    //document.getElementsByClassName("dontTouch").style.display = "inline";
+    //document.getElementsByClassName("dontTouch").style.display = "block";
     setTimeout(() => {
       firstCard.style.zIndex = "9000";
+      //this.classList.toggle("flipCard");
       secondCard.style.zIndex = "9000";
     }, 1000);
     flippedCards = 0;
@@ -95,8 +96,6 @@ function random(min, max) {
 //     i++;
 //   }
 
-//   console.log(arr);
-
 //   let arr2 = [];
 //   let j = 0;
 //   while(arr2.length < 10 ){
@@ -111,15 +110,20 @@ function random(min, max) {
 
 //   let arr3 = arr.concat(arr2);
 
-//   console.log(arr3);
-//   console.log(arr3.length);
-
 //   let index;
 //   card.forEach(card => {
-//     card.setAttribute(cardNumber) = arr3[index];
+//     card.setAttribute("cardNumber",arr3[index]);
+//     console.log(card.getAttribute("cardNumber"));
 //     card.textContent = arr3[index];
 //     index +=1;
 //   });
 
+  
 
+
+// secondCard.setAttribute("cardNumber", firstCard.getAttribute("cardNumber"));
+// console.log(secondCard.getElementsByClassName("front")); 
+
+
+//when reset -> front of cards -> change text content 
 
---
+++ jsProject/memoryGame.js
@@ -0,0 +1,100 @@
+const card = document.querySelectorAll(".card");
+const btn = document.querySelector(".button");
+let phoneNumerTxt = document.querySelector('#phoneNumberTxt'); 
+let submitBtn = document.querySelector(".submit");
+
+card.forEach(card => {
+    card.addEventListener("click", flipCard);
+});
+
+btn.addEventListener("click", shuffleCards);
+
+submitBtn.addEventListener("click", submit)
+
+let flippedCards = 0;
+let firstCard;
+let secondCard;
+
+function flipCard(){
+    if(phoneNumerTxt.textContent.length === 16){
+        submit();
+        return;
+    }
+    if(flippedCards === 0){
+        firstCard = this;
+        flippedCards += 1;
+        firstCard.classList.toggle("flipCard");
+    }
+    else{
+        secondCard = this;
+        secondCard.classList.toggle("flipCard");
+        if(firstCard.getElementsByClassName("back")[0].textContent === secondCard.getElementsByClassName("back")[0].textContent){
+            if(phoneNumberTxt.textContent.length === 3 ||  phoneNumberTxt.textContent.length === 9){
+                phoneNumberTxt.textContent += " - ";
+              }
+            phoneNumerTxt.textContent += firstCard.getElementsByClassName("back")[0].textContent;
+            console.log("match!");
+            setTimeout(() => {
+                firstCard.classList.toggle("flipCard");
+                secondCard.classList.toggle("flipCard");
+            }, 1000);
+        }
+        else{
+            console.log("not a mactch");
+            setTimeout(() => {
+                firstCard.classList.toggle("flipCard");
+                secondCard.classList.toggle("flipCard");
+            }, 1000);
+        }
+        flippedCards = 0;
+    }
+}
+
+function shuffleCards(){
+    console.log("Shuffle");
+    let arr = [];
+    let i = 0;
+    while(arr.length < 10 ){
+        let num = random(0, 9);
+        while(arr.includes(num))
+        {
+            num = random(0, 9);
+        }
+        arr[i] = num;
+        i++;
+    }
+    let arr2 = [];
+    let j = 0;
+    while(arr2.length < 10 ){
+        let num = random(0, 9);
+        while(arr2.includes(num))
+        {
+            num = random(0, 9);
+        }
+        arr2[j] = num;
+        j++;
+    }
+    let arr3 = arr.concat(arr2);
+    //ADD: for each card??
+    let count = 0;
+    card.forEach(card => {
+        card.getElementsByClassName("back")[0].textContent = arr3[count];
+        count ++;
+    });
+    phoneNumerTxt.textContent = "";
+}
+
+function submit(){
+    alert('Thank you for submitting your number: ' + phoneNumerTxt.textContent);
+    console.log("Phone Number Submitted: " + phoneNumerTxt.textContent);
+    shuffleCards();
+}
+
+function random(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num;
+}
+
+
+
+
